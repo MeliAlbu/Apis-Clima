2e05507e03014207f2b893272548de64bb4d895c
melialbu <44821933+MeliAlbu@users.noreply.github.com>
2019-05-28 16:21:06 -0300

    Clima apis

---
+++ main.js
@@ -0,0 +1,50 @@
+
+// obtengo todos los elementos que necesito modificar
+const title = document.querySelector('.movie-info .title');
+const rated = document.querySelector('#rated');
+const year = document.querySelector('#year');
+const genre = document.querySelector('#genre');
+const description = document.querySelector('.movie-info .description');
+const writtenBy = document.querySelector('.movie-info .written-by span');
+const directedBy = document.querySelector('.movie-info .directed-by span');
+const starring = document.querySelector('.movie-info .starring span');
+const image = document.querySelector('#movie img');
+
+
+
+const input = document.querySelector('#search');
+
+
+const sunMaxima = document.querySelector('.sun .max')
+const sunMinima = document.querySelector('.sun .min')
+const sunIcon = document.querySelector('.sun .icon')
+
+
+const monMaxima = document.querySelector('.mon .max')
+const monMinima = document.querySelector('.mon .min')
+const monIcon = document.querySelector('.mon .icon')
+
+
+const tueMaxima = document.querySelector('.tue .max')
+const tueMinima = document.querySelector('.tue .min')
+const tueIcon = document.querySelector('.tue .icon')
+
+const wedMaxima = document.querySelector('.wed .max')
+const wedMinima = document.querySelector('.wed.min')
+const wedIcon = document.querySelector('.wed .icon')
+
+
+const thuMaxima = document.querySelector('.thu .max')
+const thuMinima = document.querySelector('.thu .min')
+const thuIcon = document.querySelector('.thu.icon')
+
+const friMaxima = document.querySelector('.fri .max')
+const friMinima = document.querySelector('.fri .min')
+const friIcon = document.querySelector('.fri .icon')
+
+const satMaxima = document.querySelector('.sat .max')
+const satMinima = document.querySelector('.sat .min')
+const satIcon = document.querySelector('.sat .icon' .)
+
+
+
